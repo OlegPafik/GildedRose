e12312c21aa9da9b6849faa8e735cc771f064b74
Oleg <oleg.pafik@eversens.com>
2023-07-22 10:16:38 +0200

    test: add test for quality and sellIn to sulfuras

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,14 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('fixme', 0, 0)]);
+describe('Sulfuras, Hand of Ragnaros', () => {
+  it('should not change quality', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 15, 80)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].quality).toBe(80);
+  });
+  it('should not change sellIn', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 15, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(15);
   });
 });
