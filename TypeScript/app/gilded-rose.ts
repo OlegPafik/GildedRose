86bcfd3df35e59f9a6f5ee5befdde2a60f28b3d6
Oleg <oleg.pafik@eversens.com>
2023-07-22 12:15:37 +0200

    refactor: add  isAgedBrie

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -10,6 +10,8 @@ export class Item {
   }
 }
 
+const isAgedBrie = (item: Item): boolean => item.name == 'Aged Brie'
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -17,9 +19,10 @@ export class GildedRose {
     this.items = items;
   }
 
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (!isAgedBrie(this.items[i]) && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1
