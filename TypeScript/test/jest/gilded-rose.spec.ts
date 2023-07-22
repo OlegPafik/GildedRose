2284fb7200e2973802508667d081ff11adae05f0
Oleg <oleg.pafik@eversens.com>
2023-07-22 10:20:01 +0200

    test: add test for sulfuras quality when sellIn = zero and negative

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,14 +1,26 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Sulfuras, Hand of Ragnaros', () => {
-  it('should not change quality', () => {
+  it('should not change quality when sellIn positive', () => {
     const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 15, 80)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(80);
   });
+  it('should not change quality when sellIn negative', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', -15, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
+  });
+  it('should not change quality when sellIn zero', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
+  });
+
   it('should not change sellIn', () => {
     const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 15, 80)]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toBe(15);
   });
+
 });
