d0537014cd9e3738084d7af4103f44cc22884ee8
Oleg <oleg.pafik@eversens.com>
2023-07-22 10:36:24 +0200

    test: rename sulfuras

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,34 +1,35 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Sulfuras, Hand of Ragnaros', () => {
+  const Sulfuras = 'Sulfuras, Hand of Ragnaros';
   it('should not change quality when sellIn positive', () => {
-    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 15, 80)]);
+    const gildedRose = new GildedRose([new Item(Sulfuras, 15, 80)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(80);
   });
   it('should not change quality when sellIn negative', () => {
-    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', -15, 80)]);
+    const gildedRose = new GildedRose([new Item(Sulfuras, -15, 80)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(80);
   });
   it('should not change quality when sellIn zero', () => {
-    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 80)]);
+    const gildedRose = new GildedRose([new Item(Sulfuras, 0, 80)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(80);
   });
 
   it('should not change sellIn when sellIn positive', () => {
-    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 15, 80)]);
+    const gildedRose = new GildedRose([new Item(Sulfuras, 15, 80)]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toBe(15);
   });
   it('should not change sellIn when sellIn negative ', () => {
-    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', -15, 80)]);
+    const gildedRose = new GildedRose([new Item(Sulfuras, -15, 80)]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toBe(-15);
   });
   it('should not change sellIn when sellIn zero', () => {
-    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 80)]);
+    const gildedRose = new GildedRose([new Item(Sulfuras, 0, 80)]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toBe(0);
   });
